3ed1bdee73891891e6fd83417adddeb064b4e530
imad-18 <imad.labrini@uit.ac.ma>
2025-07-23 14:19:04 +0100

    client front project structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
-import TaskList from './components/TaskList';
-import './index.css'; // Make sure Tailwind CSS is imported
-
 function App() {
   return (
     <div className="App">
-      <TaskList />
+      <h1>hello dear</h1>
     </div>
   );
 }
